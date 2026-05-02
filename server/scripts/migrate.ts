79677b9ee7a0b1a294ef7d33bf3cf8a86c7489c3
Ilya Amelevich <ilya.amelevich@gmail.com>
2026-05-02 18:30:46 +0200

    Update migration script to support dynamic migration folder paths
    - Modified the `migrate.ts` script to allow the use of environment variables for specifying migration folder paths for both the main and log databases.
    - This change enhances flexibility in managing database migrations across different environments.
    
    Signed-off-by: Ilya Amelevich <ilya.amelevich@gmail.com>

--- server/scripts/migrate.ts
+++ server/scripts/migrate.ts
@@ -1,13 +1,17 @@
 import { migrate } from "drizzle-orm/bun-sqlite/migrator";
 
 import config from "../drizzle.config";
-import { db, logDb } from "../src/db";
 import logConfig from "../log.drizzle.config";
+import { db, logDb } from "../src/db";
 
 try {
-  migrate(db, { migrationsFolder: config.out });
+  migrate(db, {
+    migrationsFolder: process.env.DB_MIGRATIONS_FOLDER || config.out,
+  });
   console.log("Migration main database successful");
-  migrate(logDb, { migrationsFolder: logConfig.out });
+  migrate(logDb, {
+    migrationsFolder: process.env.LOG_MIGRATIONS_FOLDER || logConfig.out,
+  });
   console.log("Migration log database successful");
   process.exit(0);
 } catch (err) {
